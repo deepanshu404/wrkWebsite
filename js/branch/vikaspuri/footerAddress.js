e4b619a6ee1fa48385ec79a12da00cdf2c1241cb
deepanshu404 <deepsfarm.1@gmail.com>
2025-05-12 23:53:42 +0530

    DraftAdress Added the the footer

--- js/branch/vikaspuri/footerAddress.js
+++ js/branch/vikaspuri/footerAddress.js
@@ -1,3 +1,3 @@
-const branchAddress = "B-57, Ground Floor, opp. Metro Pillar Number 605, Block-B, New Krishna Park, Vikaspuri, New Delhi, Delhi, 110018";
+const branchAddress = "New Krishna Park, Vikaspuri, New Delhi, Delhi, 110018";
 document.getElementById("address").innerHTML = branchAddress;
 document.getElementById("address(1)").innerHTML = branchAddress;
