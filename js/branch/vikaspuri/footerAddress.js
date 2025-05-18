214636276172d5e120d049a35c3fdec3d8cdd584
deepanshu404 <deepsfarm.1@gmail.com>
2025-05-18 13:59:35 +0530

    Added The Headoffice Address

--- js/branch/vikaspuri/footerAddress.js
+++ js/branch/vikaspuri/footerAddress.js
@@ -1,3 +1,3 @@
-const branchAddress = "New Krishna Park, Vikaspuri, New Delhi, Delhi, 110018";
+const branchAddress = "Head Office: First Floor 32 Central Market Ashok Vihar Deep Cinema Market, New Delhi, Delhi, Pincode-110018 Vikaspuri Branch: New Krishna Park, Vikaspuri, New Delhi, Delhi, 110018";
 document.getElementById("address").innerHTML = branchAddress;
 document.getElementById("address(1)").innerHTML = branchAddress;
